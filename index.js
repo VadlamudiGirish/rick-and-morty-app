f72d23c090350928427fb7cd870ca537c3806e51
Girish Vadlamudi <girishvadlamudi@Girishs-MacBook-Pro.local>
2025-03-24 14:22:32 +0100

    Intial JS-refactor, remove some redundant code

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import { createCharacterCard } from "./components/CharacterCard/CharacterCard.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector(
-  '[data-js="search-bar-container"]'
-);
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
@@ -27,7 +24,6 @@ async function characterCard(page, searchQuery) {
   const data = await fetchCharacters(page, searchQuery);
   maxPage = data.info.pages;
   pagination.textContent = `${page}/${maxPage}`;
-  const cardContainer = document.querySelector('[data-js="card-container"]');
   cardContainer.innerHTML = "";
   const characters = data.results;
   characters.forEach((character) => {
@@ -46,19 +42,17 @@ async function characterCard(page, searchQuery) {
     cardContainer.append(cardElement);
   });
 }
-await characterCard(page, searchQuery);
-pagination.textContent = `${page}/${maxPage}`;
 
-prevButton.addEventListener("click", async () => {
+characterCard(page, searchQuery);
+
+prevButton.addEventListener("click", () => {
   page === 1 ? (page = 1) : (page = page - 1);
-  pagination.textContent = `${page}/${maxPage}`;
-  await characterCard(page, searchQuery);
+  characterCard(page, searchQuery);
 });
 
-nextButton.addEventListener("click", async () => {
+nextButton.addEventListener("click", () => {
   page === maxPage ? (page = maxPage) : (page = page + 1);
-  pagination.textContent = `${page}/${maxPage}`;
-  await characterCard(page, searchQuery);
+  characterCard(page, searchQuery);
 });
 
 searchBar.addEventListener("submit", (event) => {
