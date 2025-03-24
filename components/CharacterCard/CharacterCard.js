e357027b9501cd541e25277433f69ab415c636ab
Adam Harris <adamtjharris@gmail.com>
2025-03-24 11:46:04 +0100

    render first 20 dynamic cards

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,23 +1,23 @@
-export function createCharacterCard() {
+export function createCharacterCard(imageSource, characterName, status, characterType, occurences) {
   const liElement = document.createElement("li");
   liElement.className = "card";
   liElement.innerHTML = `<div class="card__image-container">
             <img
               class="card__image"
-              src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+              src=${imageSource}
               alt="Rick Sanchez"
             />
             <div class="card__image-gradient"></div>
           </div>
           <div class="card__content">
-            <h2 class="card__title">Rick Sanchez</h2>
+            <h2 class="card__title">${characterName}</h2>
             <dl class="card__info">
               <dt class="card__info-title">Status</dt>
-              <dd class="card__info-description">Alive</dd>
+              <dd class="card__info-description">${status}</dd>
               <dt class="card__info-title">Type</dt>
-              <dd class="card__info-description"></dd>
+              <dd class="card__info-description">${characterType}</dd>
               <dt class="card__info-title">Occurrences</dt>
-              <dd class="card__info-description">51</dd>
+              <dd class="card__info-description">${occurences}</dd>
             </dl>
           </div>`;
   return liElement;
